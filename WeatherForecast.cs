5be586ec9b732ae5d51d485a33d42b6bc9e7ca56
Osttik <lololka23@gmail.com>
2022-04-09 11:46:59 +0300

    init

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ASP._6._0.TEMPLATE
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
